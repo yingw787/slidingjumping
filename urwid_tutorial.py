61a017308a70d65bccb72d9c6c2bd054dfda1b97
Ying Wang <yingw787.personal@gmail.com>
2019-11-05 14:06:25 -0500

    Update urwid_tutorial.py.

--- urwid_tutorial.py
+++ urwid_tutorial.py
@@ -10,4 +10,9 @@ palette = [
     ('bg', 'black', 'dark blue')
 ]
 
-txt = urwid.Text(('banner', 'Hello World'))
+txt = urwid.Text(('banner', 'Hello World'), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+fill = urwid.Filler(map1)
+map2 = urwid.AttrMap(fill, 'bg')
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+loop.run()
