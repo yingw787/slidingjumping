1f6c86fe8e3bd10685584f7e5e6cd4e150773f33
Ying Wang <yingw787.personal@gmail.com>
2019-11-05 15:11:52 -0500

    Update urwid_tutorial.py.

--- urwid_tutorial.py
+++ urwid_tutorial.py
@@ -1,28 +1,71 @@
 import urwid
 
-choices = 'Chapman Cleese Gilliam Idle Jones Palin'.split()
+def menu_button(caption, callback):
+    button = urwid.Button(caption)
+    urwid.connect_signal(button, 'click', callback)
+    return urwid.AttrMap(button, None, focus_map='reversed')
+
+def sub_menu(caption, choices):
+    contents = menu(caption, choices)
+    def open_menu(button):
+        return top.open_box(contents)
+    return menu_button([caption, '...'], open_menu)
+
+def menu(title, choices):
+    body = [urwid.Text(title), urwid.Divider()]
+    body.extend(choices)
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+def item_chosen(button):
+    response = urwid.Text(['You chose ', button.label, '\n'])
+    done = menu_button('Ok', exit_program)
+    top.open_box(urwid.Filler(urwid.Pile([response, done])))
 
 def exit_program(button):
     raise urwid.ExitMainLoop()
 
-def item_chosen(button, choice):
-    response = urwid.Text(['You chose ', choice, '\n'])
-    done = urwid.Button('Ok')
-    urwid.connect_signal(done, 'click', exit_program)
-    main.original_widget = urwid.Filler(urwid.Pile([response, urwid.AttrMap(done, None, focus_map='reversed')]))
+menu_top = menu('Main Menu', [
+    sub_menu('Applications', [
+        sub_menu('Accessories', [
+            menu_button('Text Editor', item_chosen),
+            menu_button('Terminal', item_chosen)
+        ])
+    ]),
+    sub_menu('System', [
+        sub_menu('Preferences', [
+            menu_button('Appearance', item_chosen)
+        ]),
+        menu_button('Lock Screen', item_chosen)
+    ])
+])
 
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+class CascadingBoxes(urwid.WidgetPlaceholder):
+    max_box_levels = 4
 
-main = urwid.Padding(menu('Pythons', choices), left=2, right=2)
-top = urwid.Overlay(main, urwid.SolidFill('\N{MEDIUM SHADE}'),
-    align='center', width=('relative', 60),
-    valign='middle', height=('relative', 60),
-    min_width=20, min_height=9)
+    def __init__(self, box):
+        super(CascadingBoxes, self).__init__(urwid.SolidFill('/'))
+        self.box_level = 0
+        self.open_box(box)
 
+    def open_box(self, box):
+        self.original_widget = urwid.Overlay(urwid.LineBox(box),
+            self.original_widget,
+            align='center', width=('relative', 80),
+            valign='middle', height=('relative', 80),
+            min_width=24, min_height=8,
+            left=self.box_level * 3,
+            right=(self.max_box_levels - self.box_level - 1) * 3,
+            top=self.box_level * 2,
+            bottom=(self.max_box_levels - self.box_level - 1) * 2
+        )
+        self.box_level += 1
+
+    def keypress(self, size, key):
+        if key == 'esc' and self.box_level > 1:
+            self.original_widget = self.original_widget[0]
+            self.box_level -= 1
+        else:
+            return super(CascadingBoxes, self).keypress(size, key)
+
+top = CascadingBoxes(menu_top)
 urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
